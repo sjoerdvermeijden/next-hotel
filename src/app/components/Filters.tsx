242986a3c93360a4a1942be682b4ece2dfb50222
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2025-06-20 02:42:23 +0200

    Adds filterS

--- src/app/components/Filters.tsx
+++ src/app/components/Filters.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 function Filters({ }: Props) {
     return (
-        <div className='flex shrink-0 mr-2 border border-gray-300 rounded p-2 w-[250px]'>
+        <div className='flex flex-col shrink-0 mr-2 border border-gray-300 rounded p-2 w-[250px]'>
             <div>
                 <h3 className='text-2xl'>Filter op:</h3>
             </div>
@@ -12,10 +12,13 @@ function Filters({ }: Props) {
 
             </div>
             <div>
-
+                <h3 className="font-bold">Type accomodatie:</h3>
             </div>
             <div>
-
+                <h3 className="font-bold">Type accomodatie:</h3>
+            </div>
+            <div>
+                <h3 className="font-bold">Type accomodatie:</h3>
             </div>
         </div>
     )
