f48d40dee14d8451f702e92091bafdf14ce09354
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2025-06-22 21:25:09 +0200

    Updates the filters

--- src/app/components/Filters.tsx
+++ src/app/components/Filters.tsx
@@ -6,16 +6,16 @@ function Filters({ }: Props) {
     return (
         <div className='flex flex-col shrink-0 mr-2 border border-gray-300 rounded w-[250px]'>
             <div className="border-b p-2">
-                <p className='font-bold'>Filter op:</p>
+                <p className='font-bold'>Filter op</p>
             </div>
             <div className="border-b p-2">
-                <h3 className="font-bold">Type accomodatie:</h3>
+                <h3 className="font-bold">Type accomodatie</h3>
             </div>
             <div className="border-b p-2">
-                <h3 className="font-bold">Type accomodatie:</h3>
+                <h3 className="font-bold">Faciliteiten</h3>
             </div>
             <div className="p-2">
-                <h3 className="font-bold">Type accomodatie:</h3>
+                <h3 className="font-bold">Sterren</h3>
             </div>
         </div >
     )
