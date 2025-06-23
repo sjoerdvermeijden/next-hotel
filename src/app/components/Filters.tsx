ff31d777044681db0bdc5706721814f0d1b8d267
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2025-06-23 18:35:27 +0200

    Adds function for the filter toggle

--- src/app/components/Filters.tsx
+++ src/app/components/Filters.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+'use client';
+
+import React, { useState } from 'react'
 
 import { accomodation_type } from '../../../filters'
 
 function Filters() {
+    const [isActive, setIsActive] = useState<number[]>([]);
+
     return (
-        <div className='flex flex-col shrink-0 mr-3 border border-gray-300 rounded w-[250px] text-sm'>
+        <div className='flex flex-col shrink-0 mr-3 border border-gray-300 rounded w-[264px] text-sm'>
             <div className="border-b p-2">
                 <p className='font-bold'>Filter op:</p>
             </div>
@@ -21,14 +25,14 @@ function Filters() {
                         })
                     }
                 </ul>
-            </div>
+            </div >
             <div className="border-b p-2">
                 <h3 className="font-bold mb-2">Faciliteiten</h3>
                 <ul>
                     {
-                        accomodation_type.map((item) => {
-                            return <li key={item.id} className='mb-3 flex items-center hover:cursor-pointer'>
-                                <span className='inline-block w-5 h-5 border border-gray-500 mr-2 rounded-[3px]'></span>
+                        accomodation_type.map((item, index) => {
+                            return <li key={item.id} className='mb-3 flex items-center hover:cursor-pointer' onClick={() => setIsActive([...isActive, index])}>
+                                <span className={`${(isActive.indexOf(index) > -1) ? 'bg-blue-500' : 'border-gray-500'} inline-block w-5 h-5 border  mr-2 rounded-[3px]`}></span>
                                 <p className='mr-auto'>{item.label}</p>
                                 <p>28</p>
                             </li>
