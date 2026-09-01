315a62f91d2e2931dba55dd874be3ad5289f6177
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2026-09-01 08:20:52 +0200

    Corrects the wrongfull written accommodation word

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,10 +8,10 @@ function Header() {
             <div className='container max-w-[1075px] mx-auto flex items-center'>
                 <Link href="/" className='mr-auto'><h3 className="text-2xl text-white font-bold">Booking.com</h3></Link>
                 <div className='flex items-center'>
-                    <span className='font-bold text-white mr-2 transition-colors hover:bg-blue-900 px-4 py-2 rounded cursor-pointer'>Registreer je accomodatie</span>
+                    <span className='font-bold text-white mr-2 transition-colors hover:bg-blue-900 px-4 py-2 rounded cursor-pointer'>Registreer je accommodatie</span>
                     <div className='flex text-blue-500 font-bold text-sm'>
-                        <div className='bg-white mr-[10px] px-[10px] py-[7px] rounded-[3px] hover:bg-blue-50 cursor-pointer'>Registreren</div>
-                        <div className='bg-white px-[10px] py-[7px] rounded-[3px] hover:bg-blue-50 cursor-pointer'>Inloggen</div>
+                        <div className='bg-white mr-[10px] px-[9px] py-[7px] rounded-[3px] hover:bg-blue-50 cursor-pointer'>Registreren</div>
+                        <div className='bg-white px-[9px] py-[7px] rounded-[3px] hover:bg-blue-50 cursor-pointer'>Inloggen</div>
                     </div>
                 </div>
             </div>
