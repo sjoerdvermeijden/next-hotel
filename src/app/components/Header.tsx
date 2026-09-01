55ed88f16667bd850a77123b94b9b6d5748543d0
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2026-09-01 08:18:42 +0200

    Adds some stylign changes to the header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,13 +5,13 @@ import Link from 'next/link'
 function Header() {
     return (
         <div className='bg-blue-800 p-4'>
-            <div className='container mx-auto flex items-center'>
+            <div className='container max-w-[1075px] mx-auto flex items-center'>
                 <Link href="/" className='mr-auto'><h3 className="text-2xl text-white font-bold">Booking.com</h3></Link>
                 <div className='flex items-center'>
-                    <span className='font-bold text-white mr-4 transition-colors hover:bg-blue-900 px-4 py-2 rounded cursor-pointer'>Registreer je accomodatie</span>
+                    <span className='font-bold text-white mr-2 transition-colors hover:bg-blue-900 px-4 py-2 rounded cursor-pointer'>Registreer je accomodatie</span>
                     <div className='flex text-blue-500 font-bold text-sm'>
-                        <div className='bg-white mr-2 px-2 py-1 rounded-sm hover:bg-blue-50 cursor-pointer'>Registreren</div>
-                        <div className='bg-white px-2 py-1 rounded-sm hover:bg-blue-50 cursor-pointer'>Inloggen</div>
+                        <div className='bg-white mr-[10px] px-[10px] py-[7px] rounded-[3px] hover:bg-blue-50 cursor-pointer'>Registreren</div>
+                        <div className='bg-white px-[10px] py-[7px] rounded-[3px] hover:bg-blue-50 cursor-pointer'>Inloggen</div>
                     </div>
                 </div>
             </div>
